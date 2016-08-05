038d27259f1209185cd46db4cdfeccc4e2c88860
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-08-04 23:59:34 -0500

    Fake repo

--- rumbl/lib/rumbl/repo.ex
+++ rumbl/lib/rumbl/repo.ex
@@ -1,3 +1,11 @@
 defmodule Rumbl.Repo do
-  use Ecto.Repo, otp_app: :rumbl
+  # use Ecto.Repo, otp_app: :rumbl
+  alias Rumbl.User
+  def all(User) do
+    [
+      %{id: "1", name: "Héctor Patricio", email:"hectorip@shoelovers.co"},
+      %{id: "2", name: "", email:"hectorip@shoelovers.co"},
+      %{id: "3", name: "Héctor Patricio", email:"hectorip@shoelovers.co"},
+    ]
+  end
 end
---
+++ rumbl/web/models/User.ex
@@ -0,0 +1,3 @@
+defmodule Rumbl.User do
+  
+end
