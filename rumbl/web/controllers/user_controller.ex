45734de3120431e638560eb07b018ccc927e5540
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-08-09 03:01:07 -0500

    wip

--- rumbl/web/controllers/user_controller.ex
+++ rumbl/web/controllers/user_controller.ex
@@ -11,4 +11,9 @@ defmodule Rumbl.UserController do
     user = Rumbl.Repo.get(User, id)
     render conn, "show.html", user: user
   end
+
+  def new(conn, _params) do
+    changeset = User.changeset(%{})
+    render conn, "new.html", changeset: changeset
+  end
 end
