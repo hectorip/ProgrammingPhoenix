817d35d41992e9e129237a70b54b181aaa4dd521
Hector Ivan Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-09-07 23:58:21 -0500

    Seed for video categories

--- rumbl/priv/repo/seeds.exs
+++ rumbl/priv/repo/seeds.exs
@@ -9,3 +9,10 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Rumbl.Repo
+alias Rumbl.Category
+
+for category <- ~(Action Drama Romance Comedy Sci-fi Talk) do
+  Repo.insert!(%Category{name: category})
+end
