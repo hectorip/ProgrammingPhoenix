30b8ab5d251f747eaea2fdbc8e40b5f92c3e14b1
Hector Ivan Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-09-07 23:50:34 -0500

    Adding relationship in video and optional and required fields

--- rumbl/web/models/video.ex
+++ rumbl/web/models/video.ex
@@ -6,10 +6,14 @@ defmodule Rumbl.Video do
     field :title, :string
     field :description, :string
     belongs_to :user, Rumbl.User
+    belongs_to :category, Rumbl.Category
 
-    timestamps()
+    timestamps
   end
 
+  @required_fields ~w(url title description)
+  @optional_fields ~w(category_id)
+  
   @doc """
   Builds a changeset based on the `struct` and `params`.
   """
