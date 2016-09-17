7ea3a6f1ce517183ccb008758446cd7665925e54
Hector Ivan Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-09-16 23:56:13 -0500

    Keyword queries

--- rumbl/lib/ecto_practices/queries_1.ex
+++ rumbl/lib/ecto_practices/queries_1.ex
@@ -12,3 +12,12 @@ jose = Repo.one(from u un User, where: u.username == ^username)
 # ponemos mal los tipos de datos
 
 Repo.all(from u in User, where: u.username == 123)
+
+
+# Queries con syntaxis de Keyword map
+
+Repo.one from u in User,
+         select: count(u.id),
+         where: ilike(u.username, ^"%j") or
+                ilike(u.username, ^"c%")
+
