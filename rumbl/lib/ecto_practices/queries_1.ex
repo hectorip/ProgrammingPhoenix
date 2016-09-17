29eb24793bb7989e9846ae544492204177157b72
Hector Ivan Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-09-16 23:53:56 -0500

    Ecto types

--- rumbl/lib/ecto_practices/queries_1.ex
+++ rumbl/lib/ecto_practices/queries_1.ex
@@ -4,4 +4,11 @@ alias Rumbl.Repo
 
 # Qeury básico con condición
 username = "josevalim"
+
+# Repo.one regresa error si la consulta devuelve más de un resultado
 jose = Repo.one(from u un User, where: u.username == ^username)
+
+# Ecto sabe los tipos y levanta un error cuando
+# ponemos mal los tipos de datos
+
+Repo.all(from u in User, where: u.username == 123)
