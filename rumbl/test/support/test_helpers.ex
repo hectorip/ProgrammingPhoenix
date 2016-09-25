c06e2851ac89d6744e2e24d9b94be66e43382d0a
Hector Ivan Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-09-24 23:59:49 -0500

    User test helper

--- rumbl/test/support/test_helpers.ex
+++ rumbl/test/support/test_helpers.ex
@@ -1,3 +1,18 @@
 defmodule Rumbl.TestHelpers do
-  
+  alias Rumbl.Repo
+
+  def inser_user(attrs \\ %{}) do
+    changes = Dict.merge(%{
+        name: "John Doe",
+        username: "user#{Base.encode16(:crypto.rand_bytes(8))}",
+        password: "supersecret"
+    }, attrs)
+    %Rumbl.User{}
+      |> Rumbl.User.registration_changeset(changes)
+      |> Repo.insert!()
+  end
+
+  def insert_video() do
+    
+  end
 end
